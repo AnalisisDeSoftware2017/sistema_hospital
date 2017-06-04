b4ee41a93ca3694aa4ec145822ac6483f86a675f
sbogado <sbogado@nsuconsulting.com>
2017-06-04 13:21:43 -0300

    Sigue tirando errores

--- src/main/java/ar/edu/unlam/analisis/software/grupo2/controller/EnfermedadesMedicoController.java
+++ src/main/java/ar/edu/unlam/analisis/software/grupo2/controller/EnfermedadesMedicoController.java
@@ -2,8 +2,9 @@ package ar.edu.unlam.analisis.software.grupo2.controller;
 
 import ar.edu.unlam.analisis.software.grupo2.core.exception.FailedLoginException;
 import ar.edu.unlam.analisis.software.grupo2.core.model.User;
+import ar.edu.unlam.analisis.software.grupo2.core.services.MedicoService;
 import ar.edu.unlam.analisis.software.grupo2.core.services.UserService;
-import ar.edu.unlam.analisis.software.grupo2.ui.InformesForm;
+import ar.edu.unlam.analisis.software.grupo2.ui.EnfermedadesMedicoForm;
 import ar.edu.unlam.analisis.software.grupo2.ui.LoginJFrame;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,28 +13,27 @@ import org.springframework.stereotype.Controller;
  * Created by sbogado on 5/17/17.
  */
 @Controller
-public class InformeController extends AbstractFrameController<InformesForm>{
-
+public class EnfermedadesMedicoController extends AbstractFrameController<EnfermedadesMedicoForm>{
 
     private  MainMenuController mainMenuController;
-
-    private UserService userService;
+    private MedicoService userService;
+    private MedicoService medicoService;
 
     @Autowired
-    public InformeController(LoginJFrame loginJFrame, UserService userService, MainMenuController mainMenuController){
-        this.frame = loginJFrame;
-        this.userService = userService;
+    public EnfermedadesMedicoController(EnfermedadesMedicoForm enfermedadesMedicoForm, MedicoService medicoService, MainMenuController mainMenuController){
+        this.frame = enfermedadesMedicoForm;
+        this.userService = medicoService;
         this.mainMenuController = mainMenuController;
     }
 
     public void prepareAndOpenFrame() {
         frame.setVisible(true);
-        registerClickAction(frame.getButtonIngresar(), (e)->login());
+        /*registerClickAction(frame.getButtonIngresar(), (e)->login());
         registerEnterKeyAction(frame.getPasswordBox(), ()->login());
-        registerEnterKeyAction(frame.getUserNameBox(), ()->login());
+        registerEnterKeyAction(frame.getUserNameBox(), ()->login());*/
     }
 
-    public void login(){
+    /*public void login(){
         User user = new User();
         user.setUsername(frame.getUsuario());
         user.setPassword(frame.getPass());
@@ -45,7 +45,8 @@ public class InformeController extends AbstractFrameController<InformesForm>{
         } catch (FailedLoginException e) {
             e.printStackTrace();
         }
-    }
+    }*/
+
 
 
 }
